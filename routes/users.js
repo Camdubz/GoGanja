7ed498b58617618143b04ae88a84a52d9a7581cf
Camdubzc <camwhite982@gmail.com>
2023-09-24 18:22:40 +1300

    Get to over one hour 6 mins in video

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,28 @@
 import express from "express";
-
+import { 
+    updateUser, 
+    deleteUser, 
+    getUser, 
+    getUsers 
+} from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 const router = express.Router();
+
+//CHECK AUTHENTICATION
+router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+    res.send("Hello, you are logged in!")
+})
+
+//UPDATE
+router.put("/:id", updateUser);
+
+//DELETE
+router.delete("/:id", deleteUser);
+
+//GET
+router.get("/:id", getUser);
+
+//GET ALL
+router.get("/", getUsers);
 
 export default router
